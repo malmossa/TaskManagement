2bc022d4e74c599470e82bb0967c619c00e69b94
Mansor Almossa <malmossa@gmail.com>
2024-10-19 12:12:51 -0700

    Add project files.

---
+++ TaskManagement/Models/ApplicationUser.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagement.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [StringLength(255, ErrorMessage = "The FirstName should have a maximum of 255 characters.")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(255, ErrorMessage = "The LastName should have a maximum of 255 characters.")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [ValidateNever]
+        public ICollection<UserTask> UserTasks { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{LastName}, {FirstName}";
+            }
+        }
+    }
+}
---
+++ TaskManagement/Models/UserTask.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagement.Models
+{
+    public class UserTask
+    {
+        [Key]
+        public Guid TaskId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        [ValidateNever]
+        public string Status {  get; set; }
+        public string? AssignedToId { get; set; }
+
+        [ValidateNever]
+        public ApplicationUser? AssignedTo { get; set; }
+
+    }
+}
