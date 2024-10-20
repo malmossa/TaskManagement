8e7d792d20cf123cb2274e7bc0f1e4003e49407e
Mansor Almossa <malmossa@gmail.com>
2024-10-20 13:46:24 -0700

    completed

---
+++ TaskManagement/Data/AppDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TaskManagement.Models;
+
+namespace TaskManagement.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UserTask> UserTasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserTask>()
+                .HasOne(x => x.AssignedTo)
+                .WithMany(x => x.UserTasks)
+                .HasForeignKey(x => x.AssignedToId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
--- TaskManagement/Program.cs
+++ TaskManagement/Program.cs
@@ -1,7 +1,31 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TaskManagement.Data;
+using TaskManagement.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Configures the DBContext to link to the server using the ConnectionString definedin the appsettings.json 
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+
+//Locks down the whole app that will require log in and athorization
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+});
+
+//Configures the Identity settings of the project that will set the ApplicationUser class to be the identity class.
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>();
 
 var app = builder.Build();
 
@@ -17,11 +41,14 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+//This enables authentication to the APP
+app.UseAuthentication();
 app.UseAuthorization();
 
+//This sets the default page to be opened which the the LogIn page under the ApplicationUsers controller
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=ApplicationUser}/{action=Login}/{id?}");
 
 app.Run();
+
