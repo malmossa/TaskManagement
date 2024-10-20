07604336b6e22d356665bc21700307aff46912f3
Mansor Almossa <malmossa@gmail.com>
2024-10-20 09:56:51 -0700

    Completed

---
+++ TaskManagement/Models/ModelsView/ApplicationUserView.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagement.Models.ModelsView
+{
+    public class ApplicationUserView
+    {
+        [ValidateNever]
+        public string Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Remote(action: "IsEmailInUse", "ApplicationUsers")]
+        public string Email {  get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Password and confirmation do not match.")]
+        public string ConfirmPassword { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [NotMapped]
+        public string Role {  get; set; }       
+    }
+}
---
+++ TaskManagement/Models/ModelsView/LogInView.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagement.Models.ModelsView
+{
+    public class LogInView
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
+    }
+}
