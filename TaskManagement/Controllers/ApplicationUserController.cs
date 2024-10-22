a9a774b9d9f6fcc47624b6c883d633b2c8fbe46d
Mansor Almossa <malmossa@gmail.com>
2024-10-22 09:54:24 -0700

    completed

---
+++ TaskManagement/Controllers/ApplicationUserController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using TaskManagement.Models;
+using TaskManagement.Models.ModelsView;
+
+namespace TaskManagement.Controllers
+{
+    public class ApplicationUserController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public ApplicationUserController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LogInView model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Uses the default SignInManager to sign in if the user passes the authentication.
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded) 
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError("", "Invalid Login attempt");
+            }
+
+            return View(model);
+        }
+    }
+}
